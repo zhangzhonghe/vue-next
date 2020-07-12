982a856714cc2afa82636eac9ab60056917c9589
BeiYuShuiGuoLvDeKongQi <958414905@qq.com>
2020-07-12 22:59:51 +0800

    chore: about Vue entry

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -8,10 +8,13 @@ import { isString, NOOP, generateCodeFrame } from '@vue/shared'
 
 const compileCache: Record<string, RenderFunction> = Object.create(null)
 
+// 运行时的编译函数
 function compileToFunction(
   template: string | HTMLElement,
   options?: CompilerOptions
 ): RenderFunction {
+  // 如果template为一个元素节点
+  // 则返回其内部的HTML文本信息
   if (!isString(template)) {
     if (template.nodeType) {
       template = template.innerHTML
@@ -21,12 +24,16 @@ function compileToFunction(
     }
   }
 
+  // 将以编译的template缓存起来，
+  // 如之后有相同template，则直接返回
+  // 上次编译的结果
   const key = template
   const cached = compileCache[key]
   if (cached) {
     return cached
   }
 
+  // 如果template为id选择器，则查询其对应元素
   if (template[0] === '#') {
     const el = document.querySelector(template)
     if (__DEV__ && !el) {
@@ -39,6 +46,7 @@ function compileToFunction(
     template = el ? el.innerHTML : ``
   }
 
+  // 这里是真正的模板字符串，交给底层的编译函数进行编译
   const { code } = compile(template, {
     hoistStatic: true,
     onError(err: CompilerError) {
@@ -66,6 +74,9 @@ function compileToFunction(
   const render = (__GLOBAL__
     ? new Function(code)()
     : new Function('Vue', code)(runtimeDom)) as RenderFunction
+
+  // 把模板编译之后的渲染函数代码文本交给Function构造器
+  // 在运行时构造出一个渲染函数，并缓存起来。
   return (compileCache[key] = render)
 }
 
